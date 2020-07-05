236f1672036bcd9f8487bf1a3ae5682aa4ecd9d9
OleksandrVashchishak <67052863+OleksandrVashchishak@users.noreply.github.com>
2020-07-06 01:12:56 +0300

    Add files via upload

---
+++ jcuery.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+$('.block-menu').click(function(event){
+$('.block-menu,.burger-top,.burger-bot,.burger-menu-visible').toggleClass('active');   
+      
+});  
+        
+});
+
+
