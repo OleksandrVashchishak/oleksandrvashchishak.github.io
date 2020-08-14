0f0c709f0c02a4db5887de3296aec73429f1d16f
OleksandrVashchishak <67052863+OleksandrVashchishak@users.noreply.github.com>
2020-08-14 09:43:54 +0300

    Add files via upload

--- restoraunt/function.js
+++ restoraunt/function.js
@@ -5,12 +5,24 @@ $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
       items: 4,
       loop: false,
-      
+      autoHeight:true,
       navRewind: true,
       nav : true,
       navText : ["",""],
     responsive:{
         0:{
+            items:1,
+            stagePadding: 70,
+        },
+        360:{
+            items:1,
+            stagePadding: 80,
+        },
+        405:{
+            items:1,
+            stagePadding: 100,
+        },
+        685:{
             items:2,
         },
         940:{
@@ -25,7 +37,7 @@ $(document).ready(function(){
    
 });
 const slider = $('#slider');
-
+const activeEls = $('.owl-item.active');
 
 $('#arrow-lef').click(function(){
    slider.trigger('next.owl.carousel');
@@ -34,3 +46,17 @@ $('#arrow-lef').click(function(){
 $('#arrow-righ').click(function(){
    slider.trigger('prev.owl.carousel');
 });
+
+
+
+
+
+//owl.on('changed.owl.carousel', function() {
+//
+//    activeEls = $('.owl-item.active');
+//    console.log("adsd");
+//    setCarouselCaption( activeEls[1] ); 
+//
+//});
+
+// console.log(activeEls);
