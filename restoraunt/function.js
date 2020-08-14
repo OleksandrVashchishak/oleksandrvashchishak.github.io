8bb15c06cfd83e5a50f5335864d89176a48d870c
OleksandrVashchishak <67052863+OleksandrVashchishak@users.noreply.github.com>
2020-08-14 10:34:58 +0300

    Add files via upload

--- restoraunt/function.js
+++ restoraunt/function.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-   
-    
-    
   $(".owl-carousel").owlCarousel({
       items: 4,
       loop: false,
@@ -12,51 +9,66 @@ $(document).ready(function(){
     responsive:{
         0:{
             items:1,
-            stagePadding: 70,
+            stagePadding: 70
         },
         360:{
             items:1,
-            stagePadding: 80,
+            stagePadding: 80
         },
         405:{
             items:1,
-            stagePadding: 100,
+            stagePadding: 100
         },
         685:{
-            items:2,
+            items:2
         },
         940:{
-            items:3,
+            items:3
         },
         1250:{
-            items:4,
+            items:4
         }
     }
       
   });
    
 });
-const slider = $('#slider');
-const activeEls = $('.owl-item.active');
-
-$('#arrow-lef').click(function(){
-   slider.trigger('next.owl.carousel');
-});
-
-$('#arrow-righ').click(function(){
-   slider.trigger('prev.owl.carousel');
-});
 
 
 
 
-
-//owl.on('changed.owl.carousel', function() {
-//
-//    activeEls = $('.owl-item.active');
-//    console.log("adsd");
-//    setCarouselCaption( activeEls[1] ); 
-//
+//$(document).ready(function(){
+//  $(".owl-carousel-one").owlCarousel({
+//      items: 4,
+//      loop: false,
+//      autoHeight:true,
+//      navRewind: true,
+//      nav : true,
+//      navText : ["",""],
+//    responsive:{
+//        0:{
+//            items:1,
+//            stagePadding: 70,
+//        },
+//        360:{
+//            items:1,
+//            stagePadding: 80,
+//        },
+//        405:{
+//            items:1,
+//            stagePadding: 100,
+//        },
+//        685:{
+//            items:2,
+//        },
+//        940:{
+//            items:3,
+//        },
+//        1250:{
+//            items:4,
+//        }
+//    }
+//      
+//        });
+//   
 //});
-
-// console.log(activeEls);
