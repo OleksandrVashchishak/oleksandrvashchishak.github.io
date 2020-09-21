e525973e5f6ec55ec34e2a22a524ed86175ae0c6
OleksandrVashchishak <67052863+OleksandrVashchishak@users.noreply.github.com>
2020-09-21 15:11:51 +0300

    Delete main.js

--- app/js/main.js
+++
@@ -1,8 +0,0 @@
-      $(document).ready(function() {
-        $('.home__leng').click(function(event){
-        $('.home__togle-leng,.home__leng-img').toggleClass('active');   
-              
-        });  
-                
-        });
-  
