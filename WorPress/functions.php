6f87adebe279f1d645c718425eb7d7205f039b46
OleksandrVashchishak <67052863+OleksandrVashchishak@users.noreply.github.com>
2020-10-03 01:38:17 +0300

    Update functions.php

--- WorPress/functions.php
+++ WorPress/functions.php
@@ -1,7 +1,7 @@
 <?php
 
+// Підключення стилів
 add_action( 'wp_enqueue_scripts', 'timber_style' );
-
 function timber_style() {
     wp_enqueue_style( 'magnific-style', get_template_directory_uri() . '/assets/css/magnific-popup.css' );
     wp_enqueue_style( 'main-atyle', get_stylesheet_uri() );
@@ -16,10 +16,16 @@ function timber_style() {
 //     integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
 //     crossorigin="anonymous' );
 //     wp_enqueue_script( 'jquery' );
+
 //     wp_enqueue_script( 'magnific-script', get_template_directory_uri() . '/assets/js/jquery.magnific-popup.min.js',
 // array(jquery), null, true );
+
 // wp_enqueue_script( 'main-script', get_template_directory_uri() . '/assets/js/main.js',
 // array(jquery), null, true );
 // }    
+
+// Добавити кастомне лого
+
+add_theme_support( 'custom-logo' );
 
 ?>
