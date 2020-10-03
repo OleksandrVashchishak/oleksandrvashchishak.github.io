9e40cfde3ca14c9255cf316c0aaf50a42df122b3
OleksandrVashchishak <67052863+OleksandrVashchishak@users.noreply.github.com>
2020-10-04 02:16:37 +0300

    Update index.php

--- WorPress/index.php
+++ WorPress/index.php
@@ -1,30 +1,19 @@
 <?php get_header(); ?>
   <main class="main">
-    <div class="container">
     <div class="main__img">
       <img src="<?php bloginfo( 'template_url'); ?>/assets/img/main-img.png" alt="">
     </div>
   </div>
       <div class="download">
-        <img src="<?php bloginfo( 'template_url'); ?>/assets/img/home-download.png" alt="">
        <p class="download__p"><a class="download__link" href="<?php the_field('download__link')?>" download>СКАЧАТЬ ВЕСЬ КАТАЛОГ ДОМОВ</a></p>
       </div>
-      <div class="container">
-      <div class="gallery">
-        <div class="gallery__title">
-          ФОТОГРАФИИ НАШИХ РАБОТ
-        </div>
-        <div class="gallery__text ">
-          некоторые фотографии построенных нами домов и бань из бруса
-        </div>
          <div class="gallery__inner popup-gallery">
          <?php the_field('photo')?>
          </div>
       </div>
       <div class="main__img">
         <img src="<?php bloginfo( 'template_url'); ?>/assets/img/main-img.png" alt="">
-      </div>
-    </div>
+        </div>
   </main>
 
 Вивід кастомних постів:
@@ -34,8 +23,6 @@
 4.створити запис
 5. зайти в сторінку в полі релатіоншіп вивести пости
 
-
- 
   <?php
 $featured_posts = get_field('project__item'); //Вказати ту ж назву, що і в полі релатіоншіп в кастомих полях
 if( $featured_posts ): ?>
@@ -64,6 +51,27 @@ if( $featured_posts ): ?>
     wp_reset_postdata(); ?>
 <?php endif; ?>
 
+
+// Форма зворотнього звязку 
+// contact form 7
+
+    <form class="contact__form">
+      <?php echo do_shortcode( '[contact-form-7 id="69" title="Контактна форма 1"]' ); ?>        
+      </form>
+
+// Текст який писав в вордпрессі в адмінці
+
+<div class="contact__form-flex">
+<p class="ppp">[text* Name class:contact__input class:contact__input-focus placeholder "Name"]</p>
+<p class="ppp">[email* Email class:contact__input class:contact__input-focus placeholder "email"]</p>
+<p class="ppp">[text* Subject class:contact__input class:contact__input-focus placeholder "subject"]</p>
+<p class="ppp">[text Status id:inputValue class:contact__input class:input-value "status"]</p>
+  </div>
+[textarea textarea-48 class:contact__textarea "question"]
+ <div class="contact__button-container">
+        [submit class:contact__button "Send"]
+        </div>
+/////
 
 
   <?php get_footer(); ?>
