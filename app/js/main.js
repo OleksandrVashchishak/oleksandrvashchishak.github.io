01a6fac0afd275a7b707d9b6015021cf3b5e65fa
Oleksandr Vashchishak <67052863+OleksandrVashchishak@users.noreply.github.com>
2020-10-30 14:03:13 +0200

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,32 @@
-$(function(){
+let toScrollBtn = document.getElementById('toScrollBtn')
+let toScrollBtnRight = document.getElementById('toScrollBtnRight')
 
 
-
-  
-});
+let scrollElem = document.getElementById('scrollElem')
+toScrollBtn.addEventListener('click', getScroll)
+toScrollBtnRight.addEventListener('click', getScrollRight)
+toScrollBtn.addEventListener('click', hideScroll)
+function getScroll(){
+  scrollElem.scrollLeft += 200
+  if(scrollElem.scrollLeft >= 10){
+    toScrollBtnRight.style.display = 'block'
+    
+  }
+  console.log(scrollElem.scrollLeft);
+}
+function getScrollRight(){
+  scrollElem.scrollLeft -= 200
+  if(scrollElem.scrollLeft <= 210){
+    toScrollBtnRight.style.display = 'none'
+  }
+  console.log(scrollElem.scrollLeft);
+}
+function hideScroll(){
+  toScrollBtnRight.style.display = 'block'
+}
+if(scrollElem.scrollLeft <= 10){
+  toScrollBtnRight.style.display = 'none'
+}
+if(scrollElem.scrollLeft >= 10){
+  toScrollBtnRight.style.display = 'block'
+}
