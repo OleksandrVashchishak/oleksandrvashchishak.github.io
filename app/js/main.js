ccb6d36190366fb1754f7455c7685d35cb9dc8de
Oleksandr Vashchishak <q121312q@gmail.com>
2020-11-03 15:15:03 +0200

    weqwe

--- app/js/main.js
+++ app/js/main.js
@@ -1,42 +1,57 @@
-let toScrollBtn = document.getElementById('toScrollBtn')
-let toScrollBtnRight = document.getElementById('toScrollBtnRight')
- 
-let hideEat = document.querySelectorAll('.hideEat')
-let scrollElem = document.getElementById('scrollElem')
+$(document).ready(function() {
+  var allText = $('.vacancy__resume--text'),
+    textLeft = $('.vacancy__resume--text--left'),
+    textRight = $('.vacancy__resume--text--right');
+  $('.vacancy__resume--body input[type=checkbox]').on('change', function() {
+    if ($(this).is(':checked')) {
+      allText.removeClass('active');
+      textRight.addClass('active');
+    } else {
+      allText.removeClass('active');
+      textLeft.addClass('active');
+    }
+  })
 
-toScrollBtn.addEventListener('click', getScroll)
-toScrollBtnRight.addEventListener('click', getScrollRight)
-toScrollBtn.addEventListener('click', hideScroll)
-function getScroll(){
-  scrollElem.scrollLeft += 200
-  if(scrollElem.scrollLeft >= 10){
-    toScrollBtnRight.style.display = 'block'
-    
-  }
-  console.log(scrollElem.scrollLeft);
-}
-function getScrollRight(){
-  scrollElem.scrollLeft -= 200
-  if(scrollElem.scrollLeft <= 210){
-    toScrollBtnRight.style.display = 'none'
-  }
-  console.log(scrollElem.scrollLeft);
-}
-function hideScroll(){
-  toScrollBtnRight.style.display = 'block'
-}
-if(scrollElem.scrollLeft <= 10){
-  toScrollBtnRight.style.display = 'none'
-}
-if(scrollElem.scrollLeft >= 10){
-  toScrollBtnRight.style.display = 'block'
-}
+  var mobileFilter = $('.filter__mobile');
+
+  $('.vacancy__mobile-filter').on('click', function(e) {
+    e.preventDefault();
+    mobileFilter.addClass('opened');
+  })
+
+  $('.filter__back').on('click', function(e) {
+    e.preventDefault();
+    mobileFilter.removeClass('opened');
+  })
+
+  $('.vacancy__view button').on('click', function(e) {
+    e.preventDefault();
+    $('.vacancy__view button').removeClass('active');
+    $(this).addClass('active');
+  })
 
 
+  var newsList = $('.offers-block__list');
 
-for( let i=0; i<hideEat.length; i++){
-  console.log(hideEat[i])
-  if(hideEat[i].childNodes[1].childNodes[1].textContent == ''){
-    hideEat[i].style.display ='none'
-  }
-}
+  $('.firstView').on('click', function() {
+    newsList.removeClass('offers-block__list--grid');
+  })
+
+  $('.secondView').on('click', function() {
+    newsList.addClass('offers-block__list--grid');
+  })
+
+
+  $('.head__info p').each(function () {
+      $(this).prop('Counter', 0).animate({
+              Counter: $(this).data('value')
+          }, {
+          duration: 1500,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(this.Counter.toFixed());
+          }
+      });
+  });
+
+})
