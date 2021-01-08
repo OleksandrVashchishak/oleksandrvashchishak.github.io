ab54e87e53f600ee2f215db86270933c3a15029f
Oleksandr Vashchishak <67052863+OleksandrVashchishak@users.noreply.github.com>
2021-01-08 17:58:04 +0200

    Update woocommerce.php

--- WordPressHelp/woocommerce.php
+++ WordPressHelp/woocommerce.php
@@ -5,3 +5,5 @@ function mytheme_add_woocommerce_support() {
 }
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
+// remove actions. Input number in end hooc
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
