af366be6dc5adfef192912b576659b6542921f8a
Oleksandr Vashchishak <67052863+OleksandrVashchishak@users.noreply.github.com>
2021-01-08 19:26:27 +0200

    Update woocommerce.php

--- WordPressHelp/woocommerce.php
+++ WordPressHelp/woocommerce.php
@@ -1,5 +1,4 @@
 // add support woocommerce theme
-
 function mytheme_add_woocommerce_support() {
     add_theme_support( 'woocommerce' );
 }
@@ -7,3 +6,11 @@ add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
 // remove actions. Input number in end hooc
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
+
+// спочатку відключаю хук з дефолтими значеннями, а потім підключаю свій кастомний
+remove_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
+function my_custom_title(){
+	echo '<p>'.get_the_title().'</p>';
+}
+
+add_action( 'woocommerce_shop_loop_item_title', 'my_custom_title', 10 );
