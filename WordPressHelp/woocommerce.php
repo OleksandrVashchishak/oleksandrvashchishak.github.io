d3d9488b8ae1de5798d102fa80e27f95dd01cf3f
Oleksandr Vashchishak <67052863+OleksandrVashchishak@users.noreply.github.com>
2021-01-08 17:52:51 +0200

    Create woocommerce.php

---
+++ WordPressHelp/woocommerce.php
@@ -0,0 +1,7 @@
+// add support woocommerce theme
+
+function mytheme_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+
