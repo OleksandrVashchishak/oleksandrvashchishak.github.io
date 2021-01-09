b5433ebb0c60bbb2a4ad608cc18b5509d2caecbd
Oleksandr Vashchishak <67052863+OleksandrVashchishak@users.noreply.github.com>
2021-01-09 21:32:12 +0200

    Update woocommerce.php

--- WordPressHelp/woocommerce.php
+++ WordPressHelp/woocommerce.php
@@ -14,3 +14,22 @@ function my_custom_title(){
 }
 
 add_action( 'woocommerce_shop_loop_item_title', 'my_custom_title', 10 );
+
+
+
+
+// Особистий кабінет
+
+// Видаляємо пункти меню
+add_filter ( 'woocommerce_account_menu_items', 'remove_my_account_links' );
+function remove_my_account_links( $menu_links ){
+	//unset( $menu_links['edit-address'] ); // Addresses
+	//unset( $menu_links['dashboard'] ); // Remove Dashboard
+	//unset( $menu_links['payment-methods'] ); // Remove Payment Methods
+	//unset( $menu_links['orders'] ); // Remove Orders
+	//unset( $menu_links['downloads'] ); // Disable Downloads
+	//unset( $menu_links['edit-account'] ); // Remove Account details tab
+	//unset( $menu_links['customer-logout'] ); // Remove Logout link
+	return $menu_links;
+ 
+}
