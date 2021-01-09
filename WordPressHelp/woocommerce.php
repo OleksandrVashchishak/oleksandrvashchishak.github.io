24f8748b245262d3b28f711828849120946cc006
Oleksandr Vashchishak <67052863+OleksandrVashchishak@users.noreply.github.com>
2021-01-09 21:37:52 +0200

    Update woocommerce.php

--- WordPressHelp/woocommerce.php
+++ WordPressHelp/woocommerce.php
@@ -40,3 +40,33 @@ function misha_rename_downloads( $menu_links ){
 	return $menu_links;
 }
 
+//Добавляємо нову вкладку в особичтий кабінет
+// Add Link (Tab) to My Account menu
+add_filter ( 'woocommerce_account_menu_items', 'misha_log_history_link', 40 );
+function misha_log_history_link( $menu_links ){
+ 
+	$menu_links = array_slice( $menu_links, 0, 5, true ) 
+	+ array( 'log-history' => 'Log history' )
+	+ array_slice( $menu_links, 5, NULL, true );
+ 
+	return $menu_links;
+ 
+}
+
+// Register Permalink Endpoint
+add_action( 'init', 'misha_add_endpoint' );
+function misha_add_endpoint() {
+ 
+	// WP_Rewrite is my Achilles' heel, so please do not ask me for detailed explanation
+	add_rewrite_endpoint( 'log-history', EP_PAGES );
+ 
+}
+
+// woocommerce_account_{ENDPOINT NAME}_endpoint
+add_action( 'woocommerce_account_log-history_endpoint', 'misha_my_account_endpoint_content' );
+function misha_my_account_endpoint_content() {
+ 
+	// of course you can print dynamic content here, one of the most useful functions here is get_current_user_id()
+	echo 'Last time you logged in: yesterday from Safari.';
+ 
+}
