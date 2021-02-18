7b3c9d3d365f73c8367821259c45ac11494a8df7
Oleksandr Vashchishak <67052863+OleksandrVashchishak@users.noreply.github.com>
2021-02-18 09:17:18 +0200

    Update woocommerce.php

--- WordPressHelp/woocommerce.php
+++ WordPressHelp/woocommerce.php
@@ -182,7 +182,7 @@ if ( empty( $_POST[ 'billing_first_name' ] )   ) {
 return $errors;
 }
 
-// відправлення даних php з js	
+// відправлення даних php з js AJAX
 // в js	
   $.ajax({
           type: 'POST',
@@ -244,6 +244,82 @@ function misha_rename_downloads( $menu_links ){
 	return $menu_links;
 }
 
+	// отримати всі дані про замовлення по ID
+function get_order_details($order_id){
+    $order = wc_get_order( $order_id );
+    print_r($order);
+    echo '$order->order_type: ' . $order->order_type . '<br>';
+    echo '$order->id: ' . $order->id . '<br>';
+    echo '<h4>THE POST OBJECT:</h4>';
+    echo '$order->post->ID: ' . $order->post->ID . '<br>';
+    echo '$order->post->post_author: ' . $order->post->post_author . '<br>';
+    echo '$order->post->post_date: ' . $order->post->post_date . '<br>';
+    echo '$order->post->post_date_gmt: ' . $order->post->post_date_gmt . '<br>';
+    echo '$order->post->post_content: ' . $order->post->post_content . '<br>';
+    echo '$order->post->post_title: ' . $order->post->post_title . '<br>';
+    echo '$order->post->post_excerpt: ' . $order->post->post_excerpt . '<br>';
+    echo '$order->post->post_status: ' . $order->post->post_status . '<br>';
+    echo '$order->post->comment_status: ' . $order->post->comment_status . '<br>';
+    echo '$order->post->ping_status: ' . $order->post->ping_status . '<br>';
+    echo '$order->post->post_password: ' . $order->post->post_password . '<br>';
+    echo '$order->post->post_name: ' . $order->post->post_name . '<br>';
+    echo '$order->post->to_ping: ' . $order->post->to_ping . '<br>';
+    echo '$order->post->pinged: ' . $order->post->pinged . '<br>';
+    echo '$order->post->post_modified: ' . $order->post->post_modified . '<br>';
+    echo '$order->post->post_modified_gtm: ' . $order->post->post_modified_gtm . '<br>';
+    echo '$order->post->post_content_filtered: ' . $order->post->post_content_filtered . '<br>';
+    echo '$order->post->post_parent: ' . $order->post->post_parent . '<br>';
+    echo '$order->post->guid: ' . $order->post->guid . '<br>';
+    echo '$order->post->menu_order: ' . $order->post->menu_order . '<br>';
+    echo '$order->post->post_type: ' . $order->post->post_type . '<br>';
+    echo '$order->post->post_mime_type: ' . $order->post->post_mime_type . '<br>';
+    echo '$order->post->comment_count: ' . $order->post->comment_count . '<br>';
+    echo '$order->post->filter: ' . $order->post->filter . '<br>';
+    echo '<h4>THE ORDER OBJECT (again):</h4>';
+    echo '$order->order_date: ' . $order->order_date . '<br>';
+    echo '$order->modified_date: ' . $order->modified_date . '<br>';
+    echo '$order->customer_message: ' . $order->customer_message . '<br>';
+    echo '$order->customer_note: ' . $order->customer_note . '<br>';
+    echo '$order->post_status: ' . $order->post_status . '<br>';
+    echo '$order->prices_include_tax: ' . $order->prices_include_tax . '<br>';
+    echo '$order->tax_display_cart: ' . $order->tax_display_cart . '<br>';
+    echo '$order->display_totals_ex_tax: ' . $order->display_totals_ex_tax . '<br>';
+    echo '$order->display_cart_ex_tax: ' . $order->display_cart_ex_tax . '<br>';
+    echo '$order->formatted_billing_address->protected: ' . $order->formatted_billing_address->protected . '<br>';
+    echo '$order->formatted_shipping_address->protected: ' . $order->formatted_shipping_address->protected . '<br><br>';
+
+    $order_meta = get_post_meta($order_id);
+    print_r($order_meta);
+    echo '<br><br>';
+    echo '<h3>THE ORDER META DATA (Using the array syntax notation):</h3>';
+    echo '$order_meta[_order_key][0]: ' . $order_meta[_order_key][0] . '<br>';
+    echo '$order_meta[_order_currency][0]: ' . $order_meta[_order_currency][0] . '<br>';
+    echo '$order_meta[_prices_include_tax][0]: ' . $order_meta[_prices_include_tax][0] . '<br>';
+    echo '$order_meta[_customer_user][0]: ' . $order_meta[_customer_user][0] . '<br>';
+    echo '$order_meta[_billing_first_name][0]: ' . $order_meta[_billing_first_name][0] . '<br><br>';
+    echo 'And so on ……… <br><br>';
+    echo '- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - <br><br>';
+    $items = $order->get_items();
+    foreach ( $items as $item_id => $item_data ) {
+
+        echo '<h4>RAW OUTPUT OF THE ORDER ITEM NUMBER: '. $item_id .'): </h4>';
+        print_r($item_data);
+        echo '<br><br>';
+        echo 'Item ID: ' . $item_id. '<br>';
+        echo '$item_data["product_id"] <i>(product ID)</i>: ' . $item_data['product_id'] . '<br>';
+        echo '$item_data["name"] <i>(product Name)</i>: ' . $item_data['name'] . '<br>';
+
+        // Using get_item_meta() method
+        echo 'Item quantity <i>(product quantity)</i>: ' . $order->get_item_meta($item_id, '_qty', true) . '<br><br>';
+        echo 'Item line total <i>(product quantity)</i>: ' . $order->get_item_meta($item_id, '_line_total', true) . '<br><br>';
+        echo 'And so on ……… <br><br>';
+        echo '- - - - - - - - - - - - - <br><br>';
+    }
+}
+
+get_order_details(159);	
+	
+	
 //Добавляємо нову вкладку в особичтий кабінет
 // Add Link (Tab) to My Account menu
 add_filter ( 'woocommerce_account_menu_items', 'misha_log_history_link', 40 );
